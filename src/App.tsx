e112e6618164747cbd7a0dcb3f9a595fe11cf948
hhejo <litsmainjof@gmail.com>
2024-07-31 13:28:56 +0900

    feat: Init 달력 기능

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,5 @@
 import { useState } from "react";
 
-/**
- * 오늘 날짜를 yyyy-MM-dd 형식 문자열로 리턴
- */
-function getToday(): string {
-  const dateObj: Date = new Date(); // Date Object for today
-  const year: string = dateObj.getFullYear().toString(); // year
-  const month: string = `${dateObj.getMonth() + 1}`.padStart(2, "0"); // month
-  const date: string = `${dateObj.getDate()}`.padStart(2, "0"); // date
-  return `${year}-${month}-${date}`; // yyyy-MM-dd
-}
-
 function getDateString(dateObj: Date): string {
   const year: string = dateObj.getFullYear().toString(); // year
   const month: string = `${dateObj.getMonth() + 1}`.padStart(2, "0"); // month
@@ -18,67 +7,20 @@ function getDateString(dateObj: Date): string {
   return `${year}-${month}-${date}`; // yyyy-MM-dd
 }
 
+/**
+ * 오늘 날짜를 yyyy-MM-dd 형식 문자열로 리턴
+ */
+function getToday(): string {
+  const dateObj: Date = new Date(); // Date Object for today
+  return getDateString(dateObj);
+}
+
 // App
 function App() {
-  const [today, setToday] = useState(getToday);
+  // const [] = useState();
 
-  // 현재 연도, 월
-  const currentDateObj = new Date();
-  const currentYear = currentDateObj.getFullYear();
-  const currentMonth = currentDateObj.getMonth();
-
-  // const initDateObj = new Date(currentYear, currentMonth, 1);
-  // const firstDay = initDateObj.getDay();
-
-  console.log(`오늘: ${currentDateObj}`);
-
-  let copiedDateObj = new Date(currentDateObj);
-  // copiedDateObj.setDate(copiedDateObj.getDate() - 7 * 7 - 1);
-  // console.log(`학습 당일: ${copiedDateObj}`);
-  // copiedDateObj = new Date(currentDateObj);
-  // copiedDateObj.setDate(copiedDateObj.getDate() - 7 * 7);
-  // console.log(`복습 1회차: ${copiedDateObj}`);
-  // copiedDateObj = new Date(currentDateObj);
-  // copiedDateObj.setDate(copiedDateObj.getDate() - 7 * 6);
-  // console.log(`복습 2회차: ${copiedDateObj}`);
-  // copiedDateObj = new Date(currentDateObj);
-  // copiedDateObj.setDate(copiedDateObj.getDate() - 7 * 4);
-  // console.log(`복습 3회차: ${copiedDateObj}`);
-  // copiedDateObj = new Date(currentDateObj);
-  // copiedDateObj.setDate(copiedDateObj.getDate());
-  // console.log(`복습 4회차: ${copiedDateObj}`);
-
-  copiedDateObj = new Date(currentDateObj);
-  copiedDateObj.setDate(copiedDateObj.getDate() - 7 * 7 - 1);
-  copiedDateObj.setDate(copiedDateObj.getDate() - copiedDateObj.getDay());
-  console.log(copiedDateObj);
-
-  const daysArr = [];
-  let x = getDateString(copiedDateObj);
-
-  while (x !== getToday()) {
-    const obj = {
-      dateString: getDateString(copiedDateObj), // yyyy-MM-dd
-      day: `${copiedDateObj.getDay()}`, // 요일
-      isReviewDate: false,
-    };
-    daysArr.push(obj);
-    copiedDateObj.setDate(copiedDateObj.getDate() + 1);
-    x = getDateString(copiedDateObj);
-  }
-
-  console.log(daysArr);
-
-  // dateObj.setDate(dateObj.getDate() + 1);
-  // console.log(`첫 번째 복습일: ${dateObj}`);
-
-  // const currentMonth = dateObj.getMonth();
-  // console.log("currentMonth:", currentMonth);
-  // let acc = 1;
-  // while (currentMonth === dateObj.getMonth()) {
-  //   acc += 1;
-  //   dateObj.setDate(acc);
-  // }
+  const dateObj = new Date();
+  const currentYear = dateObj.getFullYear();
 
   return (
     <>
@@ -87,8 +29,8 @@ function App() {
       <input
         type="date"
         id="learningDate"
-        value={today}
-        onChange={(e) => setToday(e.target.value)}
+        // value={today}
+        // onChange={(e) => setToday(e.target.value)}
       />
       {/* <button onClick={calculateReviewDates}>계산</button> */}
 
